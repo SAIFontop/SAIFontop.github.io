a22af5097e88a8f77e903ec3591a55da74379f9b
SAIF <saif07858098@gmail.com>
2024-08-30 11:16:52 +0400

    Create checkAuth.js

---
+++ api/checkAuth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const cookie = require('cookie');
+
+module.exports = (req, res) => {
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const token = cookies.token;
+
+    if (!token) {
+        return res.json({ loggedIn: false });
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        res.json({ loggedIn: true, user });
+    } catch (error) {
+        res.json({ loggedIn: false });
+    }
+};
