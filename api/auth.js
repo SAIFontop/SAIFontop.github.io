1aa364a7f0b02d5fd1759316520dc4d6b9573c0f
SAIF <saif07858098@gmail.com>
2024-08-30 12:58:15 +0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,6 @@
 const querystring = require('querystring');
 
-// استبدل YOUR_VERCEL_APP_URL برابط تطبيق Vercel الخاص بك
-const redirect_uri = `https://sai-fontop-github-io.vercel.app/api/callback`;
+const redirect_uri = `https://sai-fontop-github-io.vercel.app/api/callback`; // تأكد من أن هذا الرابط هو نفسه في Discord Developer Portal
 
 module.exports = (req, res) => {
     const discordAuthUrl = `https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=identify`;
