12e665234cdfa53b454e773379dea492d5631853
SAIF <saif07858098@gmail.com>
2024-08-30 11:26:41 +0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -3,5 +3,5 @@ const axios = require('axios');
 const redirect_uri = `https://sai-fontop-github-io.vercel.app/api/callback`; // استبدل YOUR_VERCEL_APP_URL برابط تطبيق Vercel الخاص بك
 
 module.exports = (req, res) => {
-    res.redirect(`https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=identify`);
+    res.redirect(`https://discord.com/oauth2/authorize?client_id=1278969884834791435&permissions=8&integration_type=0&scope=bot=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=identify`);
 };
