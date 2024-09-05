c7159979170634bcaba631a5a31d17e3a592503b
SAIF <saif07858098@gmail.com>
2024-09-05 14:59:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function getIpInfo() {
 
 function getEmailInfo() {
     const email = document.getElementById("emailInput").value;
-    fetch(`https://emailrep.io/${email}?key=YOUR_API_KEY`)
+    fetch(`https://emailrep.io/${email}?key=91cd8f313e5a8c`)
         .then(response => response.json())
         .then(data => {
             document.getElementById("emailResult").innerHTML = `
