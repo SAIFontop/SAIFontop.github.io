48d21abe7f0ebee32a09c356b377fe0c2115ff4c
SAIF <saif07858098@gmail.com>
2024-09-05 14:25:07 +0400

    Delete api directory

--- api/auth.js
+++
@@ -1,9 +0,0 @@
-const querystring = require('querystring');
-
-const redirect_uri = `https://sai-fontop-github-io.vercel.app/api/callback`;
-
-module.exports = (req, res) => {
-    const discordAuthUrl = `https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=identify`;
-    
-    res.redirect(discordAuthUrl);
-};
