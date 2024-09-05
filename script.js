d445c82caea9a6d0b92e8acb5e675a97196f8302
SAIF <saif07858098@gmail.com>
2024-09-05 14:51:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function showAlert() {
     alert("Thank you for your interest in 911 Family! Stay tuned for more updates.");
 }
+
+// وظيفة لإظهار قسم "About"
+function showAbout() {
+    document.getElementById("about").style.display = "block";
+    window.location.href = "#about";
+}
