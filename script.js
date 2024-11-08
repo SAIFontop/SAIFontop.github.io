94f17d10971acab7f8cb7a66b0dba0ab91e5fe6c
SAIF <saif07858098@gmail.com>
2024-11-08 17:30:18 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// يمكنك إضافة أي تفاعلات أو وظائف JavaScript هنا
+// تفعيل الرسوم المتحركة عند التمرير
+AOS.init();
+
+// إعداد "Client ID" الخاص بك من تطبيق Discord
+const CLIENT_ID = 'YOUR_CLIENT_ID';
+const REDIRECT_URI = 'https://your-website.com/callback'; // استبدلها بعنوان URL الخاص بك
+
+// وظيفة تسجيل الدخول باستخدام Discord
+function loginWithDiscord() {
+    const discordAuthURL = `https://discord.com/api/oauth2/authorize?client_id=${1304436734117019680}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify`;
+    window.location.href = discordAuthURL;
+}
