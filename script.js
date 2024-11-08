f36a5cda8eede2b1c7b11cb3a3a1ca4498109287
SAIF <saif07858098@gmail.com>
2024-11-08 17:41:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const REDIRECT_URI = 'https://911bot.org/callback'; // استبدلها بعنو
 
 // وظيفة تسجيل الدخول باستخدام Discord
 function loginWithDiscord() {
-    const discordAuthURL = `https://discord.com/api/oauth2/authorize?client_id=${1304436734117019680}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify`;
+    const discordAuthURL = `https://discord.com/oauth2/authorize?client_id=1304436734117019680&redirect_uri=https%3A%2F%2F911bot.org%2Fcallback&response_type=code&scope=identify
+`;
     window.location.href = discordAuthURL;
 }
