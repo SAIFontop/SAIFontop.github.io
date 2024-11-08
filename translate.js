2d9905b9a927a243bbdee7b21bf243fde66543e0
SAIF <saif07858098@gmail.com>
2024-11-08 12:51:26 +0400

    Delete translate.js

--- translate.js
+++
@@ -1,9 +0,0 @@
-const langButtons = document.querySelectorAll(".lang-btn");
-
-langButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const lang = button.getAttribute("data-lang");
-        // هنا تقوم بإضافة منطق ترجمة الصفحات بناءً على اللغة المختارة
-        alert("تم تغيير اللغة إلى " + lang);
-    });
-});
